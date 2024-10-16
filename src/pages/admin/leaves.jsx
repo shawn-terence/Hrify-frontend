9fed444a3341a22e66cfeca4d91ef42698a60d94
terence <terenceshawn18@outlook.com>
2024-10-16 12:30:37 +0300

    Feat:Added employee leaves page to admin

---
+++ src/pages/admin/leaves.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AdLeaves from '../../components/uLeaves'
+const Leaves = () => {
+  return (
+    <div>
+      <AdLeaves/>
+    </div>
+  )
+}
+
+export default Leaves
