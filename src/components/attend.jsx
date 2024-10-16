13a333035eddbf87be80d781b1cc8a814c4b7cb6
terence <terenceshawn18@outlook.com>
2024-10-16 12:12:49 +0300

    Feat:Added clock in component to components

---
+++ src/components/attend.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button } from '@nextui-org/button';
+import { Card, CardBody } from '@nextui-org/react';
+import axios from 'axios';
+import '../styles/app.css'
+const Attend = ({ onAttendanceRefresh }) => {
+  const token =localStorage.getItem('token')
+  const handleClockIn = async () => {
+    try {
+      await axios.post('http://localhost:8000/time-in/', {}, {
+        headers: {
+          'Authorization': `Token ${token}`,
+        },
+      });
+      onAttendanceRefresh(); // Refresh attendance after clocking in
+    } catch (error) {
+      console.error("Error clocking in", error);
+    }
+  };
+
+  const handleClockOut = async () => {
+    try {
+      await axios.post('http://localhost:8000/time-out/', {}, {
+        headers: {
+          'Authorization': `Token ${token}`,
+        },
+      });
+      onAttendanceRefresh(); // Refresh attendance after clocking out
+    } catch (error) {
+      console.error("Error clocking out", error);
+    }
+  };
+
+  return (
+    <div className='mt-2 p-2'>
+      <Card>
+        <CardBody className='grid grid-cols-2 gap-4 mt-2'>
+          <button className='btnM' onClick={handleClockIn}>Clock In</button>
+          <button className='btnM' onClick={handleClockOut}>Clock Out</button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Attend;
