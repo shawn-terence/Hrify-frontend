33c236e0b74dcca59108479789ef5d181ddd674d
terence <terenceshawn18@outlook.com>
2024-10-16 12:24:31 +0300

    Feat:created Employeedashboard to employee

---
+++ src/pages/employee/dashboard.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+import Projects from '../../components/projects';
+import Attend from '../../components/attend';
+import AttendanceC from '../../components/attendance';
+import Schedule from '../../components/schedule';
+import ProjectsList from '../../components/projects';
+import UserProjectsTable from '../../components/userProjectsTable';
+const Dashboard = () => {
+  const token=localStorage.getItem('token')
+
+  console.log(token)
+  const [user,setUser]=useState([])
+  return (
+    <>
+      <div className='home-container    flex-col items-center'>
+        <div>
+        <p className='text-4xl text-center mt-2 font-bold'>Home Page</p>
+          <div className='attendance-container'>
+            <AttendanceC/>
+          </div>
+          <div className='project-container'>
+            <p className='font-bold text-center text-lg'>My ongoing projects</p>
+            <UserProjectsTable/>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
+
