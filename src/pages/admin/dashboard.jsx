ec9d64c11ddf09a0b123b89dc18392761eb43138
terence <terenceshawn18@outlook.com>
2024-11-12 08:13:41 +0300

    Feat:Added total employees

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import Attend from '../../components/attend'
-import AdminReports from '../../components/admReports'
-const dashboard = () => {
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Attend from '../../components/attend';
+import AdminReports from '../../components/admReports';
+import { Card,CardHeader,CardBody } from '@nextui-org/react';
+const Dashboard = () => {
+  const [employeeCount, setEmployeeCount] = useState(0);
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/users/', {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        });
+        const users = response.data;
+        setEmployeeCount(users.length);
+      } catch (error) {
+        console.error('Error fetching users list', error);
+        alert('Failed to load employee count.');
+      }
+    };
+
+    fetchUsers();
+  }, [token]);
+
   return (
     <div>
-      <p className='text-center font-bold text-3xl'>Home Page</p>
+      <p className="text-center font-bold text-3xl">Home Page</p>
       <div>
         <Attend />
       </div>
+      <Card className="text-center">
+        <div>
+          <p>Total Employees: {employeeCount}</p>
+        </div>
+      </Card>
       <div>
         <AdminReports />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default dashboard
+export default Dashboard;
