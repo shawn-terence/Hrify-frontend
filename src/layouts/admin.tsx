c740fe8f64afe82972163b139c0dd1cad281f6b1
terence <terenceshawn18@outlook.com>
2025-06-10 07:55:25 +0300

    chore:fixing adnav

--- src/layouts/admin.tsx
+++ src/layouts/admin.tsx
@@ -1,8 +1,4 @@
-
-
-
-
-import Admninmvt from"../components/adminmvt"
+import  Navbar  from "../components/empnavbar";
 export default function AdminLayout({
   children,
 }: {
@@ -10,10 +6,10 @@ export default function AdminLayout({
 }) {
   return (
     <div className="relative flex flex-row  h-screen">
+      <Navbar/>
       <main id="mainCont" className="container   mx-auto lg:mx-36   border shadow-lg rounded-xl mt-10 max-w-7xl px-6  flex-grow pt-8 pb-8" >
         {children}
       </main>
-      <Admninmvt/>
       <footer className="w-full flex items-center justify-center py-3">
       <p className="text-lg font-bold text-gray-500">Hrify 2025</p>
       </footer>
